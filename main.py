09d7e2b1a7266b3ff8665e67648c42f34b5fbde0
Benvorth <benjamin.steinvorth@gmail.com>
2024-10-09 11:15:47 +0200

    change(): Log

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import logging
 from fastapi import FastAPI
 
 from api import alive, pdf2md
 
 app = FastAPI()
+
+
+# turn off log-spamming for each "get alive"
+logging.basicConfig(level=logging.INFO)
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+
+
+class ExcludeHealthCheckFilter(logging.Filter):
+    def filter(self, record):
+        # Exclude the /alive endpoint from logs
+        return "/pdf2md/api/common/alive/" not in record.getMessage()
+
 
 app.include_router(alive.router)
 app.include_router(pdf2md.router)
