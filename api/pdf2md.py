196bb2271731e9b1367fb70255a1a75671244dad
Benvorth <benjamin.steinvorth@gmail.com>
2024-10-09 10:58:21 +0200

    fix():

--- api/pdf2md.py
+++ api/pdf2md.py
@@ -2,9 +2,9 @@ import pymupdf4llm
 import requests
 import os
 import uuid
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from api.requests import Pdf2MdRequest
+from api.my_requests import Pdf2MdRequest
 
 router = APIRouter()
 
@@ -16,19 +16,26 @@ async def convert_pdf_url_to_md(rq: Pdf2MdRequest):
 
 def do_convert(rq: Pdf2MdRequest):
 
-    # download pdf to local temp folder
     random_filename = f"{uuid.uuid4()}.pdf"
     tmp_filename_and_path = os.path.join("/tmp/dl/", random_filename)
-    response = requests.get(rq.pdf_url)
-    with open(tmp_filename_and_path, 'wb') as f:
-        f.write(response.content)
 
-    # do extraction
-    md_texts = pymupdf4llm.to_markdown(tmp_filename_and_path, page_chunks=True)
-    the_texts = [elem.get('text') for elem in md_texts]  # list of pages
+    try:
+        # download pdf to local temp folder
+        response = requests.get(rq.pdf_url)
+        with open(tmp_filename_and_path, 'wb') as f:
+            f.write(response.content)
 
-    # clean up
-    os.remove(tmp_filename_and_path)
+        # do extraction
+        md_texts = pymupdf4llm.to_markdown(tmp_filename_and_path, page_chunks=True)
+        the_texts = [elem.get('text') for elem in md_texts]  # list of pages
 
-    return " ".join(the_texts)
+        # clean up
+        os.remove(tmp_filename_and_path)
+
+        return " ".join(the_texts)
+    except Exception as e:
+        # Cleanup if an error occurs
+        if os.path.exists(tmp_filename_and_path):
+            os.remove(tmp_filename_and_path)
+        raise HTTPException(status_code=500, detail=str(e))
 
